4b8c5db10ab6b9c18d7d09b6cfdfe6ebd0855ac3
theUmeshC <umesh.c@celestialsys.com>
2022-09-12 18:26:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const path = require("path")
+const app = express();
+const port = 8000;
+
+
+app.get('/',(req,res)=>{
+    res.sendFile('index.html',{root:__dirname});
+
+});
+
+
+
+
+app.get('/page2',(req,res)=>{
+    res.sendFile('page2.html',{root:__dirname});
+});
+app.listen(port,()=>{
+    console.log(`now listening on port ${port}`);
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
