6dbe2bed4d171c1dad7c25500ca569922547cbf4
theUmeshC <umesh.c@celestialsys.com>
2022-09-13 14:20:11 +0530

    branch commit with updates

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ const path = require("path")
 const app = express();
 const port = 8000;
 
-
 app.get('/',(req,res)=>{
     res.sendFile('index.html',{root:__dirname});
 
 });
 
-
-
-
 app.get('/page2',(req,res)=>{
     res.sendFile('page2.html',{root:__dirname});
 });
+
+
 app.listen(port,()=>{
     console.log(`now listening on port ${port}`);
 })
+
 
 app.use(express.static(path.join(__dirname, 'public')));
